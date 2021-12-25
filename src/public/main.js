cbfc8e347d08988f9aea947638d0f15f7e2566a1
Snorkungen <51717522+Snorkungen@users.noreply.github.com>
2021-12-25 20:35:44 +0200

    ADDED client fetch server file

--- src/public/main.js
+++ src/public/main.js
@@ -1,3 +1,27 @@
 const ROOT = document.getElementById("root");
 
-console.log(ROOT);
+async function wait(timeIn_ms) {
+    return new Promise((resolve) => setTimeout(resolve, timeIn_ms))
+}
+
+async function streamFile(url) {
+    const response = await fetch(url);
+    const { body } = response;
+    const reader = body.getReader();
+
+    while (true) {
+        const { value, done } = await reader.read();
+        const string = new TextDecoder("utf-8").decode(value);
+
+        string.split("").forEach(async (char) => {
+            await wait(10);
+            ROOT.textContent += char;
+        })
+
+        if (done) {
+            break;
+        }
+    }
+}
+
+streamFile("/api/streamfile/2");
