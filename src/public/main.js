fd09bc7d260794e6136c2f5c796e973220f2be85
Snorkungen <51717522+Snorkungen@users.noreply.github.com>
2021-12-26 11:05:10 +0200

    Redid Clent js

--- src/public/main.js
+++ src/public/main.js
@@ -9,17 +9,21 @@ async function streamFile(url) {
     const { body } = response;
     const reader = body.getReader();
 
+    const utf8 = new TextDecoder("utf-8");
+
     while (true) {
         const { value, done } = await reader.read();
-        const string = new TextDecoder("utf-8").decode(value);
+        if (done || !value) break;
 
-        string.split("").forEach(async (char) => {
-            await wait(10);
-            ROOT.textContent += char;
-        })
+        const charPairs = [];
+        for (let i = 0; i < value.length; i++) {
+            if (i % 2 !== 0) continue;
+            const uintArr = new Uint8Array(2);
+            uintArr[0] = value[i];
+            uintArr[1] = value[i + 1];
 
-        if (done) {
-            break;
+            await wait(0);
+            ROOT.textContent += utf8.decode(uintArr);
         }
     }
 }
